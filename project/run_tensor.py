46f1af3e5516fa95e1fa85212b6f91d4a9c50ff4
Mihika <mj434@cornell.edu>
2024-10-22 00:36:52 -0400

    All tests passing

--- project/run_tensor.py
+++ project/run_tensor.py
@@ -4,18 +4,43 @@ Be sure you have minitorch installed in you Virtual Env.
 """
 
 import minitorch
+from minitorch import tensor
 
 # Use this function to make a random parameter in
 # your module.
+
 def RParam(*shape):
     r = 2 * (minitorch.rand(shape) - 0.5)
     return minitorch.Parameter(r)
 
-# TODO: Implement for Task 2.5.
-
 def default_log_fn(epoch, total_loss, correct, losses):
     print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
 
+class Network(minitorch.Module):
+    def __init__(self, hidden_layers):
+        super().__init__()
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
+
+    def forward(self, x):
+        one = self.layer1.forward(x).relu()
+        two = self.layer2.forward(one).relu()
+        return self.layer3.forward(two).sigmoid()
+
+class Linear(minitorch.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.weights = RParam(in_size, out_size)
+        self.bias = RParam(out_size)
+
+    def forward(self, x):
+        size, feats = x.shape
+        output_dim = self.weights.value.shape[1]
+        reshaped_x = x.view(size, feats, 1)
+        reshaped_weights = self.weights.value.view(1, feats, output_dim)
+        bias_term = self.bias.value
+        return (reshaped_x * reshaped_weights).sum(dim=1).view(size, output_dim) + bias_term
 
 class TensorTrain:
     def __init__(self, hidden_layers):
